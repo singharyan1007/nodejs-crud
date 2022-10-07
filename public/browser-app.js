b7fa4200ffe9a180d0cce47d0d68325e329a330d
Aryan Singh <singh.aryan837@gmail.com>
2022-10-07 16:08:20 +0530

    New changes

--- public/browser-app.js
+++ public/browser-app.js
@@ -85,3 +85,34 @@ formDOM.addEventListener('submit', async (e) => {
     formAlertDOM.classList.remove('text-success')
   }, 3000)
 })
+
+
+function toggleBar(){
+  var nav=document.querySelector('.navbar');
+  nav.classList.toggle('responsive');
+}
+
+const toggleSwitch = document.querySelector('.theme-switch input[type="checkbox"]');
+
+function switchTheme(e) {
+    if (e.target.checked) {
+        document.documentElement.setAttribute('data-theme', 'dark');
+        localStorage.setItem('theme','dark');
+    }
+    else {
+        document.documentElement.setAttribute('data-theme', 'light');
+        localStorage.setItem('theme','light')
+    }    ;
+
+};
+
+toggleSwitch.addEventListener('change', switchTheme, false);
+const currentTheme=localStorage.getItem('theme')?localStorage.getItem('theme'):null;
+
+if (currentTheme) {
+  document.documentElement.setAttribute('data-theme', currentTheme);
+
+  if (currentTheme === 'dark') {
+      toggleSwitch.checked = true;
+  }
+}
